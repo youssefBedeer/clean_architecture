b4a7ce03ae997d38f84352eb63018d4159df1bd1
YoussefBedeer <yousser1bedeer@gmail.com>
2026-06-29 18:39:38 +0300

    - user_repository

---
+++ lib/features/user/domain/repositories/user_repository.dart
@@ -0,0 +1,7 @@
+import 'package:clean_architecture/core/errors/failure.dart';
+import 'package:clean_architecture/features/user/domain/entities/user_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class UserRepository {
+  Future<Either<Failure, UserEntity>> getUser();
+}
